4fc74f7a07ed0227e33af5deeab8e001a0676fb9
narinsak.u <nawongsak@gmail.com>
2026-06-04 17:59:43 +0700

    fix: backend docker-compose (db only), AppModule controllers, Dockerfile CMD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
 
 @Module({
   imports: [AuthModule, DbModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
