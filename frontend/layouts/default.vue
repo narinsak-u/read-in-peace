2673344a4d64937eff88020b4417a2065048fa8f
narinsak.u <nawongsak@gmail.com>
2026-06-15 10:55:20 +0700

    refactor: strip default layout to minimal for landing page only

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
 import { Toaster } from 'vue-sonner';
-import { useBooksStore } from '~/stores/books';
-
-const route = useRoute();
-const booksStore = useBooksStore();
-const isNotIndex = computed(() => route.name !== 'index');
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col">
-    <main class="flex-1">
-      <slot />
-    </main>
-    <Footer v-if="isNotIndex" />
-    <AdminFab v-if="isNotIndex" />
-    <BookFormModal
-      v-if="isNotIndex && booksStore.showForm"
-      :book="booksStore.editingBook"
-      @close="booksStore.closeForm()"
-      @saved="booksStore.closeForm()"
-    />
-    <CheckoutDrawer v-if="isNotIndex" />
+  <div>
+    <slot />
     <Toaster richColors position="top-center" />
   </div>
 </template>
